7996eb9dff66cf15c4fb430630a8e1b632a5c12d
adamp <adam.poitras97@gmail.com>
2023-11-06 08:27:51 -0500

    CRUD Records

--- website/views.py
+++ website/views.py
@@ -63,21 +63,35 @@ def delete_record(request,pk):
         delete_it = Record.objects.get(id=pk)
         delete_it.delete()
         messages.success(request, "Record deleted successfully")
-        return render('home')
+        return redirect('home')
     else:
         messages.success(request, "You must be logged in to view that page")
         return redirect('home')
 
 
 def add_record(request):
+    form = AddRecordForm(request.POST or None)
     if request.user.is_authenticated:
-        form = AddRecordForm(request.POST or None)
+
         if request.method =="POST":
             if form.is_valid():
                 add_record = form.save()
                 messages.success(request,"Record Added")
-                redirect('home')
+                return redirect('home')
         return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
+
+def edit_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Edit saved succcessfully")
+            return redirect('home')
+        return render(request,'edit_record.html', {'form': form})
     else:
         messages.success(request, "You must be logged in to view that page")
         return redirect('home')
